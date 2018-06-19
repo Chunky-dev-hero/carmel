20cc6e00533d6bbb6f56e3958793b1c15fd12fee
AndyComan <andi@fluidtrends.com>
2018-06-19 17:36:58 +0300

    Clean the template, add new page file

--- chunks/index.desktop.js
+++ chunks/index.desktop.js
@@ -1,5 +1,3 @@
 // AUTO-GENERATED FILE. PLEASE DO NOT MODIFY. CHUNKY WILL CRY.
 
 export { default as intro } from './intro/index.desktop'
-export { default as challenges } from './challenges/index.desktop'
-export { default as auth } from './auth/index.desktop'
---
+++ chunks/intro/screens/new-page.web.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Screen } from 'react-dom-chunky'
+
+export default class NewScreen extends Screen {
+  components() {
+    return super.components().concat([])
+  }
+}
