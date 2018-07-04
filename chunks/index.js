9bc5a27eb341028ea09bff8fccd76ecda649b188
AndyComan <andi@fluidtrends.com>
2018-07-04 09:15:38 +0300

    REmove auth

--- chunks/index.js
+++ chunks/index.js
@@ -1,4 +1,3 @@
 // AUTO-GENERATED FILE. PLEASE DO NOT MODIFY. CHUNKY WILL CRY.
 
 export { default as intro } from './intro'
-export { default as auth } from './auth'
--- chunks/index.web.js
+++ chunks/index.web.js
@@ -1,4 +1,3 @@
 // AUTO-GENERATED FILE. PLEASE DO NOT MODIFY. CHUNKY WILL CRY.
 
 export { default as intro } from './intro/index.web'
-export { default as auth } from './auth/index.web'
