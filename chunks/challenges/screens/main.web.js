d69e035a3fd4c1ad8d4953a0562a53a5d0a9482d
IosifDolha <iosifdolha@yahoo.com>
2018-07-23 15:15:53 +0300

    Changed the Quest's into Challenges

--- chunks/challenges/screens/main.web.js
+++ chunks/challenges/screens/main.web.js
@@ -27,17 +27,17 @@ export default class MainChallengesScreen extends Screen {
 
   componentDidMount() {
     super.componentDidMount()
-    // this.importRemoteData(this.props.index).then(challenges => {
-    //   this.setState({
-    //     challenges,
-    //     loading: false
-    //   })
+     this.importRemoteData(this.props.index).then(challenges => {
+      this.setState({
+        challenges,
+        loading: false
+      })
+     })
+    // const challenges = this.importData("challenges")
+    // this.setState({
+    //   challenges,
+    //   loading: false
     // })
-    const challenges = this.importData("challenges")
-    this.setState({
-      challenges,
-      loading: false
-    })
   }
 
   renderContent() {
